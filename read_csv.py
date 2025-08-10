fbfa6befe7860b69c0351305b0794510046720cb
varunmuriyanat <varunmuriyanat@gmail.com>
2025-08-10 16:49:40 -0400

    changed the save format to parquet and partition by year

--- read_csv.py
+++ read_csv.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import year, to_date
 
 # Create Spark session
 spark = SparkSession.builder \
@@ -6,9 +7,14 @@ spark = SparkSession.builder \
     .master("local[*]") \
     .getOrCreate()
 
-df = spark.read.csv("customers_100.csv")
+df = spark.read.csv("customers_100.csv", header=True, inferSchema=True)
+
+# 'DateJoined' is in 'yyyy-MM-dd' format
+df = df.withColumn("year", year(to_date(df["DateJoined"])))
 
 df.show()
 
-spark.stop()
+# Write to Parquet, partitioned by
+df.write.partitionBy("year").parquet("customers_parquet", mode="overwrite")
 
+spark.stop()
